b7be5e3718c83b70762ecf8ead00d54e25b812e9
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-04-11 10:32:27 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,12 @@ import { PureCell } from '@alfalab/core-components/pure-cell';
 import { Steps } from '@alfalab/core-components/steps';
 import { Tag } from '@alfalab/core-components/tag';
 import { Typography } from '@alfalab/core-components/typography';
-import { CheckmarkMIcon } from '@alfalab/icons-glyph/CheckmarkMIcon';
 import { ChevronDownMIcon } from '@alfalab/icons-glyph/ChevronDownMIcon';
 import { ChevronUpMIcon } from '@alfalab/icons-glyph/ChevronUpMIcon';
 import { OutsideMIcon } from '@alfalab/icons-glyph/OutsideMIcon';
 import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import img1 from './assets/1.png';
-import img2 from './assets/2.png';
-import img3 from './assets/3.png';
-import img4 from './assets/4.png';
-import img5 from './assets/5.png';
-import img6 from './assets/6.png';
 import hb from './assets/hb.png';
-import pds from './assets/pds.png';
 import pers from './assets/pers.png';
 import piec from './assets/piec.png';
 import rubd from './assets/rubd.png';
@@ -32,46 +24,6 @@ import { appSt } from './style.css';
 import { ThxLayout } from './thx/ThxLayout';
 import { sendDataToGACalc } from './utils/events';
 import { round } from './utils/round';
-
-const slides = [
-  {
-    title: 'Получать пассивный доход от накоплений',
-    img: img1,
-  },
-  {
-    title: 'Накопить на образование детей',
-    img: img2,
-  },
-  {
-    title: 'Выйти на пенсию досрочно',
-    img: img3,
-  },
-  {
-    title: (
-      <>
-        Больше
-        <br />
-        путешествовать
-      </>
-    ),
-    img: img4,
-  },
-  {
-    title: 'Купить дом и жить у моря на пенсии',
-    img: img5,
-  },
-  {
-    title: 'Создать начальный капитал для детей',
-    img: img6,
-  },
-];
-
-const checks = [
-  'Застрахованы на сумму до 2,8 млн ₽ в Агентстве по страхованию вкладов',
-  'Полностью наследуются на этапе накопления или выплат (кроме назначения пожизненной выплаты)',
-  'Не делятся при разводе, на них не может быть наложено взыскание и арест',
-  'Негосударственный пенсионный фонд (НПФ) гарантирует безубыточность вложений на горизонте каждых пяти лет',
-];
 
 const chipsIncome = [
   {
@@ -228,25 +180,6 @@ export const App = () => {
           </Typography.Text>
         </div>
 
-        <div>
-          <Typography.TitleResponsive tag="h2" view="small" font="system" weight="medium" style={{ marginBottom: '1rem' }}>
-            Копите на любые цели и мечты
-          </Typography.TitleResponsive>
-
-          <Swiper spaceBetween={12} slidesPerView="auto">
-            {slides.map((slide, index) => (
-              <SwiperSlide key={index} className={appSt.swSlide}>
-                <div className={appSt.slideContainer}>
-                  <img src={slide.img} width={212} height={114} alt="" style={{ marginTop: '-32px' }} />
-                  <Typography.Text view="primary-small" weight="bold" style={{ margin: '0 16px 16px' }}>
-                    {slide.title}
-                  </Typography.Text>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-
         <Typography.TitleResponsive tag="h2" view="small" font="system" weight="medium">
           Плюсы программы:
         </Typography.TitleResponsive>
@@ -378,46 +311,6 @@ export const App = () => {
             </Typography.Text>
           </span>
         </Steps>
-
-        <PureCell className={appSt.pds}>
-          <PureCell.Content>
-            <PureCell.Main>
-              <Typography.Text
-                view="primary-small"
-                weight="bold"
-                tag="p"
-                defaultMargins={false}
-                style={{ marginBottom: '0.25rem' }}
-              >
-                Альфа-Вклад с ПДС
-              </Typography.Text>
-              <Typography.Text view="secondary-large">
-                Получите повышенную ставку по вкладу с программой долгосрочных сбережений
-              </Typography.Text>
-            </PureCell.Main>
-          </PureCell.Content>
-          <PureCell.Graphics verticalAlign="center">
-            <img src={pds} width={90} height={74} alt="pds" />
-          </PureCell.Graphics>
-        </PureCell>
-
-        <div>
-          <Typography.TitleResponsive tag="h3" view="small" font="system" weight="medium" style={{ marginBottom: '.5rem' }}>
-            Деньги под защитой
-          </Typography.TitleResponsive>
-          {checks.map((check, index) => (
-            <PureCell key={index} verticalPadding="compact">
-              <PureCell.Graphics verticalAlign="center">
-                <CheckmarkMIcon color="#0D9336" />
-              </PureCell.Graphics>
-              <PureCell.Content>
-                <PureCell.Main>
-                  <Typography.Text view="primary-medium">{check}</Typography.Text>
-                </PureCell.Main>
-              </PureCell.Content>
-            </PureCell>
-          ))}
-        </div>
 
         <div>
           <Typography.TitleResponsive tag="h3" view="small" font="system" weight="medium" style={{ marginBottom: '1rem' }}>
