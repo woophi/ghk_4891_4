496db69414d54af2113178148c134e6580a7700f
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-04-11 12:09:49 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -300,7 +300,7 @@ export const App = () => {
               color="secondary"
               onClick={() => {
                 window.gtag('event', '4891_income_var4');
-                window.location.replace('alfabank://longread?endpoint=v1/adviser/longreads/46688');
+                window.location.replace('alfabank://longread?endpoint=v1/adviser/longreads/48915');
               }}
               style={{
                 color: '#2A77EF',
